4a7e0ac694adc25cf245b7ef15ddda2438e45acf
Martin Bauer <m.bauer@student.tugraz.at>
2020-04-26 19:56:04 +0200

    added dual accuracy to script.

--- calc_accuracy.r
+++ calc_accuracy.r
@@ -45,3 +45,27 @@ dat$accuracy <- dat$total_observations.TRUE / (dat$total_observations.TRUE+dat$t
 
 print(dat)
 write.csv(dat,file="data_accuracy_p_participant.csv",row.names=FALSE,na="")
+
+dat <- read.csv("all_data.csv")
+
+unique_prob_codes <- unique(dat$prob_code)
+
+dat <- dat %>% filter(practice == FALSE)  %>% 
+    group_by(prob_code,correct_dual)   %>% summarise( total_observations=n() ) 
+#    group_by(prob_code,mode,dualered,distance,correct_dual)   %>% summarise( total_observations=n() ) 
+
+
+
+dat <- data.frame(dat)
+
+#dat <- reshape(dat, idvar=c("prob_code","mode","dualered","distance"), timevar = c("correct_dual"), direction="wide")
+dat <- reshape(dat, idvar=c("prob_code"), timevar = c("correct_dual"), direction="wide")
+
+
+# replace na values with 0
+dat$total_observations.FALSE[is.na(dat$total_observations.FALSE)] <- 0
+dat$total_observations.TRUE[is.na(dat$total_observations.TRUE)] <- 0
+dat$accuracy <- dat$total_observations.TRUE / (dat$total_observations.TRUE+dat$total_observations.FALSE)
+
+print(dat)
+write.csv(dat,file="data_accuracy_p_participant_dual.csv",row.names=FALSE,na="")
