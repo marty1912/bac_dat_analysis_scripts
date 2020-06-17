d39d44827ac556cad54e695fc35673dd118670d2
Martin Bauer <m.bauer@student.tugraz.at>
2020-06-17 15:34:34 +0200

    updated README.

--- README.txt
+++ README.txt
@@ -6,15 +6,34 @@ in this folder you can find all the scripts used to analize the data for group 1
 
 if you are on mac or on linux you can run the shellscript "merge_and_zip.sh" by exexuting it in a terminal.
 type:
+
 ```
 ./merge_and_zip.sh
 ```
 
-
 the shell script will execute a few R scripts that create the data files we need for our analysis.
 afterwards all data files are added to an zip archive.
 
+if you are using windows you can install the ubuntu subsystem for windows or similar from the microsoft store after you enable developer mode.
+
+## the following scripts are used: 
+ - merge.r: merges all the files from the data folder into one big .csv
+ - calc_accuracy.r: calculates the accuracy for each participant.
+ - clean_data.r: cleans data (removes based on demographics,...)
+ - calc_rev_dist.r: adds reverse distance effect to the data and finishes cleaning (only complete cases are used)
+
+## The final (useful) datafiles are:
+ - data_accuracy.csv: has all the data on accuracy. Also from participants who are excluded from the final analysis.
+ - data_accuracy_p_participant_dual.csv: has data on accuracy for the dual tasks (visual and phonological). 
+ - data_cleaned_complete.csv: has all data of complete cases in long format (for R).
+ - data_rev_dist_effect_1rpP.csv: has data on reverse distance effect for rt, acc and ies in wide format (for jamovi).
+
+## Analysis
+
+After you run ./merge_and_zip.sh you can do the analysis using the R markdown script an_final.Rmd. 
+To create a html analysis file open it with R studio and press the knit button.
 
 
-
-
+# Requirements
+for the merge and zip part tidyverse is required.
+for the analysis you need R 4.0 and a few libraries (just check in the beginning of the file)
--- calc_accuracy.r
+++ calc_accuracy.r
@@ -31,35 +31,7 @@ dat$total_observations.FALSE[is.na(dat$total_observations.FALSE)] <- 0
 dat$total_observations.TRUE[is.na(dat$total_observations.TRUE)] <- 0
 dat$accuracy <- dat$total_observations.TRUE / (dat$total_observations.TRUE+dat$total_observations.FALSE)
 
-print(dat)
 write.csv(dat,file="data_accuracy.csv",row.names=FALSE,na="")
-d_for_anova <- dat
-
-
-############################################################################################################
-# 
-############################################################################################################
-dat <- d
-unique_prob_codes <- unique(dat$prob_code)
-
-dat <- dat %>% filter(practice == FALSE)  %>% 
-    group_by(prob_code,correct_num)   %>% summarise( total_observations=n() ) 
-
-
-dat$total_observations[is.na(dat$total_observations)] <- 0
-
-dat <- data.frame(dat)
-
-dat <- reshape(dat, idvar=c("prob_code"), timevar = c("correct_num"), direction="wide")
-
-
-# replace na values with 0
-dat$total_observations.FALSE[is.na(dat$total_observations.FALSE)] <- 0
-dat$total_observations.TRUE[is.na(dat$total_observations.TRUE)] <- 0
-dat$accuracy <- dat$total_observations.TRUE / (dat$total_observations.TRUE+dat$total_observations.FALSE)
-
-print(dat)
-write.csv(dat,file="data_accuracy_p_participant.csv",row.names=FALSE,na="")
 
 ############################################################################################################
 # get dual task accuracy.
@@ -68,7 +40,6 @@ dat <- d
 
 dat <- dat %>% filter(practice == FALSE) %>% filter(mode == "dual_vis" | mode == "dual_phon")  %>%
     group_by(prob_code,mode,correct_dual)   %>% summarise( total_observations=n() ) 
-#    group_by(prob_code,mode,dualered,distance,correct_dual)   %>% summarise( total_observations=n() ) 
 
 
 dat$total_observations[is.na(dat$total_observations)] <- 0
@@ -85,6 +56,5 @@ dat$total_observations.TRUE[is.na(dat$total_observations.TRUE)] <- 0
 dat$accuracy <- dat$total_observations.TRUE / (dat$total_observations.TRUE+dat$total_observations.FALSE)
 
 
-print(dat)
 write.csv(dat,file="data_accuracy_p_participant_dual.csv",row.names=FALSE,na="")
 
